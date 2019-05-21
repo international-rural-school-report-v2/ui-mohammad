7317d6a4c6f1c0587f4b03ff20f65a622f6bc672
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2019-05-21 14:33:35 -0700

    Added github links to team members

--- js/team.js
+++ js/team.js
@@ -1,33 +1,69 @@
 // Team Member Component
 class TeamMember {
-  constructor(name, title, img) {
+  constructor(name, title, img, github) {
     this.name = name;
     this.title = title;
     this.img = img;
+    this.github = github;
     this.HTML = `<div class="team-member">
     <div class="member-img">
       <img src="${this.img}" />
     </div>
     <h3>${this.name}</h3>
     <p>${this.title}</p>
-  </div>`
+    <a class='button' target="_blank" href="${this.github}">GitHub</a>
+  </div>`;
   }
-  inject (parentElement){
+  inject(parentElement) {
     parentElement.insertAdjacentHTML("beforeend", this.HTML);
   }
 }
 
 const list = document.querySelector(".team-members");
 
-const james = new TeamMember('James Page', 'Team Lead', 'img/team/team-james.png');
-const mohammad = new TeamMember('Mohammad Tourjoman', 'UI Developer', "img/team/coverPhoto.jpg");
-const michael = new TeamMember('Michael Hart', 'UI Developer', "img/team/team-michael.png");
-const kenneth = new TeamMember('Kenneth Ridenour', 'Frontend/React', "img/team/team-ken.jpeg");
-const matt = new TeamMember('Matt Poloni', 'Backend Developer', "img/team/team-matt.png");
+let team = [];
 
+team.push(
+  new TeamMember(
+    "James Page",
+    "Team Lead",
+    "img/team/team-james.png",
+    "https://github.com/jamespagedev"
+  )
+);
+team.push(
+  new TeamMember(
+    "Mohammad Tourjoman",
+    "UI Developer",
+    "img/team/coverPhoto.jpg",
+    "https://github.com/mtourj"
+  )
+);
+team.push(
+  new TeamMember(
+    "Michael Hart",
+    "UI Developer",
+    "img/team/team-michael.png",
+    "https://github.com/fromthehart"
+  )
+);
+team.push(
+  new TeamMember(
+    "Kenneth Ridenour",
+    "Frontend/React",
+    "img/team/team-ken.jpeg",
+    "https://github.com/ken1286"
+  )
+);
+team.push(
+  new TeamMember(
+    "Matt Poloni",
+    "Backend Developer",
+    "img/team/team-matt.png",
+    "https://github.com/matt-poloni"
+  )
+);
 
-james.inject(list);
-mohammad.inject(list);
-michael.inject(list);
-kenneth.inject(list);
-matt.inject(list);
+team.forEach(member => {
+  member.inject(list);
+});
